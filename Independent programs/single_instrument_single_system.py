6c49df138ce87037278a826effb0000d36657b7f
gaurik_shah <gaurik_shah@yahoo.co.uk>
2020-12-14 11:24:58 +0530

    Base version of backtesting project by GShah

--- Independent programs/single_instrument_single_system.py
+++ Independent programs/single_instrument_single_system.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from pathlib import Path
 
-from Strategies import assymetric_system as system
+from Strategies import rsi50-55sma_system as system
 from Trade_Analysis import trade_distribution, trade_summary, rolling_12m_trade_summary
 from Trade_Analysis import walkforward_annual_summary
 from PNL_Generation import pnl_generation as pg
@@ -20,15 +20,15 @@ if __name__ == '__main__':
     price_data.columns = ["Open", "High", "Low", "Close", "Volume"]
     price_data.index = pd.to_datetime(price_data.index, format="%d-%m-%Y")
 
-    underlying_name = "Results/Assymetric_Monthly"+"/NZ1 Index_5-20"
+    underlying_name = "Results/rsi50-55sma"+"/NZ1 Index"
 
-    period_1=5
-    period_2=20
+    period_1=55
+    period_2=14
 
     initial_capital = 1000000
     trading_cost = 0.0000
 
-    trades, price_signal = system.assymetric_system(price_data, period_1,period_2,period="M")
+    trades, price_signal = system.sma_crossover_system(price_data, period_1,period="")
 
     #trades, price_signal = wma20_macd_system.wma20_macd_system(price_data, period_1, period="M")
 
--- Strategies/rsi50-55sma_system.py
+++ Strategies/rsi50-55sma_system.py
@@ -1,20 +1,21 @@
-from Technical_Indicators import sma as sma
+from Technical_Indicators import sma as sma, rsi as rsi
 from Timeframe_Manipulation import series_resampling as series_resampling
 from Trade_Generation import trade_close as tc
 from Trade_Generation import creating_individual_trade
 
 import numpy as np
 
-def sma_crossover_system(price_data, period1, period="",trade_type="Both_leg", underlying_instrument_data=None):
-    period_sma = str(period1) + "_SMA"
+def sma_crossover_system(price_data, period_sma, period_rsi, period="",trade_type="Both_leg", underlying_instrument_data=None):
+    period_sma = str(period_sma) + "_SMA"
+    period_rsi=str(period_rsi) + "_RSI"
 
     if period=="":
         price_period=price_data
     else:
         price_period=series_resampling.price_series_periodic(price_data,period)
 
-
-    sma.sma(price_period, period1)
+    sma.sma(price_period, period_sma)
+    rsi.rsi(price_period, period_rsi)
 
     price_signal_period = price_period
 
@@ -22,7 +23,8 @@ def sma_crossover_system(price_data, period1, period="",trade_type="Both_leg", u
 
     #price_signal_period[period_sma] = sma_1
 
-    price_signal_period["Signal"] = np.where((price_signal_period["Close"]>price_signal_period[period_sma]), 1, -1)
+    price_signal_period["Signal"] = np.where((price_signal_period["Close"]>price_signal_period[period_sma]) & (price_signal_period["rsi"]>50)
+                                             , 1, np.where((price_signal_period["Close"]<price_signal_period[period_sma]) & (price_signal_period["rsi"]<50),-1,0))
 
     price_signal["Signal"] = price_signal_period["Signal"].resample("D").ffill()
     price_signal["Signal"].fillna(0, inplace=True)
